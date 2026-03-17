3f0be93be1f4922639f37cf288273a32e99d9c62
manudeep-18 <129201030+manudeep-18@users.noreply.github.com>
2026-03-17 22:10:18 +0530

    ALL ARE PERFECT

--- customer_segmentation_app/app.py
+++ customer_segmentation_app/app.py
@@ -690,3 +690,6 @@ def download_report(format):
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+#------------------- For DEBUG MODE like VS Code...etc-----------#
+if __name__ == "__main__":
+    app.run(debug=True)
